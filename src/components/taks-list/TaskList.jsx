619e5b73995d69ccc3399be00f13052c87264745
Rimantas <belauzas@gmail.com>
2024-08-08 13:20:03 +0300

    Task form actions

--- src/components/taks-list/TaskList.jsx
+++ src/components/taks-list/TaskList.jsx
@@ -1,7 +1,26 @@
 import { Task } from "./Task";
 
 export function TaskList() {
-    const data = [1, 1, 1, 1, 1, 1];
+    const data = [
+        {
+            text: 'Reikia issivalyti dantis',
+        },
+        {
+            text: 'Reikia nusiprausti veidi',
+        },
+        {
+            text: 'Reikia pasigaminti gerima',
+        },
+        {
+            text: 'Reikia sestis/stotis prie PC',
+        },
+        {
+            text: 'Reikia sukodinti krepsinio lentos uzduoti',
+        },
+        {
+            text: 'Reikia isspresti su kolegomis klausima del, "Kaip paviesinti reactini projekta per Github Pages?"',
+        },
+    ];
 
     if (data.length === 0) {
         return (
@@ -13,7 +32,7 @@ export function TaskList() {
 
     return (
         <div className="list">
-            {data.map((item, index) => <Task key={index} />)}
+            {data.map((item, index) => <Task key={index} data={item} />)}
         </div>
     );
 }
